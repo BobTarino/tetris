181bd21b931419c06326ea624305ce2d38d5036b
Bobby T <bob.tarino@gmail.com>
2022-01-21 16:28:40 -0600

    scaffolding and created main tetris file with stage display and startButton components

---
+++ react-tetris/src/components/Stage.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+import Cell from './Cell';
+
+const Stage = ({ stage }) => (
+    // create cells from stage prop
+    <div><Cell /></div>
+)
+
+export default Stage; 
