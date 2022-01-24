987276c21df636ecf816dee64453d3a247dfa8a2
Bobby T <bob.tarino@gmail.com>
2022-01-24 11:30:10 -0600

    rendered stage

--- src/components/Stage.js
+++ src/components/Stage.js
@@ -3,9 +3,10 @@ import React from 'react';
 import Cell from './Cell';
 
 const Stage = ({ stage }) => (
-    // create cells from stage prop
+    // create cells from stage prop 
     <div>
         {/* map over stage prop */}
+        {/* for each cell we will render out cell component */}
         {stage.map(row => row.map((cell, x) => <Cell key = {x} type = {cell[0]} />))}
     </div>
 );
