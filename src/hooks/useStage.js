a1830b7aa515dd5e345a5339bb9f33434fe8a4a6
Bobby T <bob.tarino@gmail.com>
2022-02-16 16:54:58 -0600

    set up stage logic with useStage custom hook

--- src/hooks/useStage.js
+++ src/hooks/useStage.js
@@ -1,1 +1,10 @@
  // stage logic 
+ import { useState } from "react";
+ import { createStage } from "../gameHelpers";
+
+ export const useStage = () => {
+    //  call useStage hook and generate initial stage aka the clean board
+     const [stage, setStage] = useState(createStage());
+
+     return [stage, setStage];
+ }
