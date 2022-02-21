cd166a3aa5c494e27172e56b3298aa106e278efd
Bobby T <bob.tarino@gmail.com>
2022-02-21 13:03:15 -0600

    disabled strict modeto fix collission bug

--- src/hooks/useStage.js
+++ src/hooks/useStage.js
@@ -1,44 +1,43 @@
- // stage logic 
- import { useState, useEffect } from "react";
- import { createStage } from "../gameHelpers";
+// state logic
+import { useState, useEffect } from 'react';
+import { createStage } from '../gameHelpers';
 
- export const useStage = (player, resetPlayer) => {
-    //  call useStage hook and generate initial stage aka the clean board
-     const [stage, setStage] = useState(createStage());
-    // call useEffect hook for side effect
-    
+export const useStage = (player, resetPlayer) => {
+    const [stage, setStage] = useState(createStage())
+
     useEffect(() => {
-        const updateStage = prevStage => {
-            // First clear the stage
-            const newStage = prevStage.map(row => 
-                row.map(cell => (cell[1] === 'clear' ? [0, 'clear'] : cell)),
-            );
 
-            // then draw the tetromino
+        const updateStage = prevStage => {
+            //flush the stage
+            const newStage = prevStage.map(row =>
+                row.map(cell => (cell[1] === 'clear' ? [0, 'clear'] : cell)),
+            )
+
+            // draw the tetrimino
             player.tetromino.forEach((row, y) => {
                 // loop through tetromino and look for what cells are occupied to determine shape
                 row.forEach((value, x) => {
-                    if(value !== 0) {
-                        // this will give us coordinates on the stage
+                    // this will give us coordinates on the stage
+                    if (value !== 0) {
                         newStage[y + player.pos.y][x + player.pos.x] = [
                             value,
                             // if set to merge we will know it has collided and we should keep tertomino in stage
-                            `${player.collided ? 'merged' : 'clear'}`,
-                        ];
+                            `${player.collided ? 'merged' : 'clear'}`
+                        ]
                     }
                 });
             });
-            // then check if we collided
+            //check if we collided
             if (player.collided) {
                 resetPlayer();
             }
 
             return newStage;
-        }; 
+        }
 
-        
-        setStage(prev => updateStage(prev));
-       }, [player, resetPlayer]); // dependencies for useEffect
+        setStage(prev => updateStage(prev))
+    }, [player, resetPlayer]);
 
-     return [stage, setStage];
- };
+    return [stage, setStage];
+
+}
