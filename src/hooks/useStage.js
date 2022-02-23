f732ec74fcee1ccd7710457c63b7e3f6e2f7280f
Bobby T <bob.tarino@gmail.com>
2022-02-23 09:45:54 -0600

    used unshift method within sweepRows function and set to useState to keep track of rows cleared

--- src/hooks/useStage.js
+++ src/hooks/useStage.js
@@ -4,9 +4,24 @@ import { createStage } from '../gameHelpers';
 
 export const useStage = (player, resetPlayer) => {
     const [stage, setStage] = useState(createStage())
-    const [rowsCleared, setRowsCleared] = useState(0);
+    const [rowsCleared, setRowsCleared] = useState(0); // keep track of all the rows cleared
 
     useEffect(() => {
+        setRowsCleared(0);
+
+        const sweepRows = newStage =>
+            newStage.reduce((ack, row) => {
+                // check if row doesn't contain zeroes
+                if (row.findIndex(cell => cell[0] === 0) === -1) { // clear row if -1 is returned 
+                    setRowsCleared(prev => prev + 1);
+                    ack.unshift(new Array(newStage[0].length).fill([0,'clear'])); // accumulator uses unshift to add new rows to array when rows are cleared, creating illusion
+                    return ack;
+                } 
+                ack.push(row); // if we dont have full row, just return row as it is in array
+                return ack;
+
+            }, [])
+
 
         const updateStage = prevStage => {
             //flush the stage
@@ -31,6 +46,7 @@ export const useStage = (player, resetPlayer) => {
             //check if we collided
             if (player.collided) {
                 resetPlayer();
+                return sweepRows(newStage);
             }
 
             return newStage;
